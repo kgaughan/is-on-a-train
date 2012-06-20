43c59b1e0c88d599138d4a956f937877449bfa4d
Keith Gaughan <k@stereochro.me>
2012-06-20 10:55:21 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import isonatrain
+
+setup(
+    name='isonatrain',
+    version=isonatrain.__version__,
+    description="Generates files based on trigger text in Twitter streams.",
+    long_description=open('README').read(),
+    license=open('LICENSE').read(),
+    url='https://github.com/kgaughan/is-on-a-train/',
+    install_requires=[line.rstrip() for line in open('requirements.txt')],
+    py_modules=['isonatrain'],
+
+    entry_points={
+        'console_scripts': [
+            'isonatrain = isonatrain:main'
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+
+    author=isonatrain.__author__,
+    author_email=isonatrain.__email__,
+    maintainer=isonatrain.__author__,
+    maintainer_email=isonatrain.__email__
+)
