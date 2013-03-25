67ba38773fbdc049c81d2d96297038f954e7b188
Keith Gaughan <k@stereochro.me>
2013-03-25 02:42:57 +0000

    OAuth is now required everywhere.

--- isonatrain.py
+++ isonatrain.py
@@ -4,17 +4,17 @@ bases on certain trigger phrases containing a matching piece of text.
 """
 
 
+from ConfigParser import RawConfigParser
+import logging
 import os.path
 import sys
-import logging
-import time
-import socket
-from ConfigParser import RawConfigParser
-from tweepy import api, StreamListener, Stream, BasicAuthHandler
-from pystache import render
+import httplib
+
+import pystache
+import tweepy
 
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 __author__ = 'Keith Gaughan'
 __email__ = 'k@stereochro.me'
 
@@ -24,7 +24,7 @@ USAGE = "Usage: %s <config>"
 LOG = logging.getLogger(__name__)
 
 
-class Listener(StreamListener):
+class Listener(tweepy.StreamListener):
     """
     Reacts to events from the Streaming API.
     """
@@ -55,11 +55,12 @@ class Listener(StreamListener):
         Generates an output file for the given screen name with the given
         message and writes it to disc.
         """
+        page = pystache.render(self.templates[screen_name], message=message)
         with open(self.output[screen_name], 'w+') as fh:
-            fh.write(render(self.templates[screen_name], message=message))
+            fh.write(page)
 
 
-def get_user_ids(screen_names):
+def get_user_ids(api, screen_names):
     """
     Queries Twitter for the User IDs corresponding to the given screen names.
     """
@@ -71,7 +72,13 @@ def read_config(parser):
     Extract the configuration from a parsed configuration file, checking that
     the data contained within is valid.
     """
-    auth = dict((k, parser.get('auth', k)) for k in ('username', 'password'))
+    keys = (
+        'consumer_key',
+        'consumer_secret',
+        'access_token_key',
+        'access_token_secret',
+    )
+    auth = dict((k, parser.get('auth', k)) for k in keys)
     templates = dict(parser.items('templates'))
     output = dict(parser.items('output'))
     if sorted(templates.keys()) != sorted(output.keys()):
@@ -137,22 +144,26 @@ def main():
 
     templates = load_templates(template_paths)
 
+    auth_handler = tweepy.OAuthHandler(
+        auth['consumer_key'],
+        auth['consumer_secret'])
+    # XXX: If these are none, do the OAuth dance.
+    auth_handler.set_access_token(
+        auth['access_token_key'],
+        auth['access_token_secret'])
+
     LOG.info("Initialising stream...")
-    stream = Stream(
-        BasicAuthHandler(auth['username'], auth['password']),
+    api = tweepy.API(auth_handler)
+    stream = tweepy.Stream(
+        auth_handler,
         Listener(templates, output, triggers),
         secure=True)
     try:
         LOG.info("Running filter")
-        ids = get_user_ids(triggers.keys())
-        while True:
-            try:
-                stream.filter(follow=ids)
-            except socket.error:
-                LOG.exception("Connectivity problem; sleeping...")
-                time.sleep(5)
+        stream.filter(follow=get_user_ids(api, triggers.keys()))
     except KeyboardInterrupt:
         pass
+    return 0
 
 
 if __name__ == '__main__':
